47de593d12b08f9d50e0a31fea0ddba36f9fe0e5
Durugo Ikechukwu <durugoobiefule2009@gmail.com>
2026-05-21 17:04:16 +0100

    Started drum kit project

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,14 @@
+// let totalDrum = document.querySelectorAll(".drum").length;
+
+// for(let i=0; i<totalDrum; i++){    
+//     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+//     alert("Just clicked")})
+// }
+
+
+const audio = document.getElementById("#Audio");
+const btn = document.getElementsByClassName(".w");
+
+btn.addEventListener("click", function(){
+    audio.play()})
+
